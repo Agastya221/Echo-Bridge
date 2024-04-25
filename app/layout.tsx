2ac91303e3a6dc7aedc77d1b6d2ff76461f9681d
agastya221 <agastyash213@gmail.com>
2024-04-25 12:01:36 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,47 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import "@stream-io/video-react-sdk/dist/css/styles.css";
+import { Analytics } from "@vercel/analytics/react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "react-datepicker/dist/react-datepicker.css";
+import { Toaster } from "react-hot-toast";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+   title: "EchoBridge",
+   description: "EchoBridge is a video call , audio app for Users.",
+};
+
+export default function RootLayout({
+   children,
+}: Readonly<{
+   children: React.ReactNode;
+}>) {
+   return (
+      <html lang="en">
+         <ClerkProvider
+            appearance={{
+               layout: {
+                  socialButtonsVariant: "iconButton",
+                  logoImageUrl: "/icons/logo-no-background.svg",
+               },
+               variables: {
+                  colorText: "#fff",
+                  colorPrimary: "#0E78F9",
+                  colorBackground: "#1C1F2E",
+                  colorInputBackground: "#252A41",
+                  colorInputText: "#fff",
+               },
+            }}
+         >
+            <body className={`${inter.className} bg-dark-1`}>
+               <Toaster />
+               {children}
+               <Analytics />
+            </body>
+         </ClerkProvider>
+      </html>
+   );
+}
